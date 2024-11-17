1f1bf41dc309a748aaa6fd8ead18f8c23deab2bd
m0stafa_darwe4 <mostafadarwesh610@gmail.com>
2024-11-17 16:49:46 +0200

    move assets into src package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {Button} from "primeng/button";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {MessageService} from "primeng/api";
+import {fontAwesomeIcons} from "../shared/font-awesome-icons";
+import {ToastService} from "./layout/toast.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'airbnb-clone-front';
+  faIconLibrary = inject(FaIconLibrary);
+  // isListingView = true;
+  // toastService = inject(ToastService);
+  // messageService = inject(MessageService);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+    //this.listenToastService();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  // private listenToastService() {
+  //   this.toastService.sendSub.subscribe({
+  //     next: newMessage => {
+  //       if (newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
+  //         this.messageService.add(newMessage);
+  //       }
+  //     }
+  //   })
+  // }
 }
