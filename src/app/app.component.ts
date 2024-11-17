3f6fe55ca6887f18efe71fb872aabe5ed328ce45
m0stafa_darwe4 <mostafadarwesh610@gmail.com>
2024-11-17 19:12:25 +0200

    add navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,37 +3,42 @@ import {RouterOutlet} from '@angular/router';
 import {Button} from "primeng/button";
 import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "../shared/font-awesome-icons";
+import {ToastModule} from "primeng/toast";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
+import {ToastService} from "./layout/navbar/toast.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Button, FontAwesomeModule],
+  imports: [RouterOutlet, Button, FontAwesomeModule, ToastModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'airbnb-clone-front';
   faIconLibrary = inject(FaIconLibrary);
-  // isListingView = true;
-  // toastService = inject(ToastService);
-  // messageService = inject(MessageService);
+  isListingView = true;
+  toastService = inject(ToastService);
+  messageService = inject(MessageService);
 
   ngOnInit(): void {
     this.initFontAwesome();
-    //this.listenToastService();
+    this.listenToastService();
   }
 
   private initFontAwesome() {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
   }
 
-  // private listenToastService() {
-  //   this.toastService.sendSub.subscribe({
-  //     next: newMessage => {
-  //       if (newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
-  //         this.messageService.add(newMessage);
-  //       }
-  //     }
-  //   })
-  // }
+  private listenToastService() {
+    this.toastService.sendSub.subscribe({
+      next: newMessage => {
+        if (newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
+          this.messageService.add(newMessage);
+        }
+      }
+    })
+  }
 }
