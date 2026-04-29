57a7de4eae38f2e7077c0ce612daac965c7e0a9b
Mostafa Darwesh <mostafadarwesh610@gmail.com>
2026-04-29 03:21:52 +0300

    Set Auth0 SPA client ID for airbnb-clone-angular-client

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   API_URL: "//localhost:4200/api",
   auth0: {
     domain: "airbnb-backend.us.auth0.com",
-    clientId: "REPLACE_WITH_SPA_CLIENT_ID",
+    clientId: "OfZNK1kcvfk4a5ROV5FVOeELFlpGvfmm",
     audience: "https://airbnb-api",
   },
 };
